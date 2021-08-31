cd9a6e9c30c3230c0d6999dbf24e17d78d941e02
Aqshola <aqsholafid2@gmail.com>
2021-08-31 10:09:51 +0700

    add search feature in homepage

--- src/script/components/home/header.js
+++ src/script/components/home/header.js
@@ -4,6 +4,7 @@ class headerHome extends HTMLElement {
   }
   connectedCallback() {
     this.render()
+    this.searchRecipe()
   }
 
   render() {
@@ -23,6 +24,7 @@ class headerHome extends HTMLElement {
         <p>Cari Resep masak untuk memasak maupun belajar memasak</p>
         <label class="search-recipe" for="recipe-search">
           <input type="search" placeholder="Cari Resep" id="recipe-search" />
+          <button id="header-search-button" aria-label="cari resep">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="20px"
@@ -36,6 +38,7 @@ class headerHome extends HTMLElement {
               clip-rule="evenodd"
             />
           </svg>
+          </button>
         </label>
         <p>
           atau lihat
@@ -45,6 +48,17 @@ class headerHome extends HTMLElement {
         
       </section>
         `
+  }
+
+  searchRecipe() {
+    const searchVal = document.querySelector('#recipe-search')
+    const searchButon = document.querySelector('#header-search-button')
+
+    searchButon.addEventListener('click', () => {
+      if (searchVal !== '' && searchVal !== ' ') {
+        location.assign(`/search.html?resep=${searchVal.value}`)
+      }
+    })
   }
 }
 
