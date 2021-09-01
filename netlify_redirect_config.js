7591c5fb0d39013e8602c63a820f2415b753ff4b
AqsholA <52363719+Aqshola@users.noreply.github.com>
2021-09-01 09:41:19 +0700

    Update netlify_redirect_config.js

--- netlify_redirect_config.js
+++ netlify_redirect_config.js
@@ -5,3 +5,9 @@ fs.copyFile('_redirects', './dist/_redirects', (err) => {
   if (err) throw err
   console.log('_redirects was copied to dist folder!')
 })
+
+fs.copyFile('_headers', './dist/_headers', (err) => {
+  if (err) throw err
+  console.log('_headers was copied to dist folder!')
+})
+
